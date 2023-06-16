3d9af3432feaa993592e41bdb2e89b218a4d7ebf
VPcume-Dev <vinhphucflash@gmail.com>
2023-06-16 15:53:23 +0700

    added shooting

--- Obj.py
+++ Obj.py
@@ -2,20 +2,38 @@ from pygame import*
 
 
 class Bullet:
-    def __init__(self, x, y, bullet_distance):
-        self.rect = Rect(x, y, 4, 2)
+    def __init__(self, x, y, direction, bullet_distance = None):
+        self.rect = Rect(x, y, 4, 4)
         self.speed = 600
         self.distance = bullet_distance
-    
+        self.direction = direction
+    def update(self, delta):
+        if self.direction == "right":
+            self.rect.x += self.speed * delta
+        elif self.direction == "left":
+            self.rect.x -= self.speed * delta
+        elif self.direction == "up":
+            self.rect.y -= self.speed * delta
+        elif self.direction == "down":
+            self.rect.y += self.speed * delta
 
 
 class Player:
     def __init__(self,x,y) -> None: # setting player
         self.rect = Rect(x,y,40,40)
         self.sped = 300
+        self.direction = "up"
     def update(self, delta):
         keys = key.get_pressed()
         movex = keys[K_RIGHT] - keys[K_LEFT]
         movey = keys[K_DOWN] - keys[K_UP]
+        if movex > 0:
+            self.direction = "right"
+        elif movex < 0:
+            self.direction = "left"
+        if movey > 0:
+            self.direction = "down"
+        elif movey < 0:
+            self.direction = "up"
         self.rect.x += movex * delta * self.sped
         self.rect.y += movey * delta * self.sped
--- game.py
+++ game.py
@@ -5,6 +5,7 @@ from functions import*
 window = display.set_mode([800,600])
 display.set_caption("Cyberstrike Chronicles")
 plr = Player(0,0) # set player pos and declare var
+bullets = []
 
 gameRunnning = True
 while gameRunnning:
@@ -12,7 +13,13 @@ while gameRunnning:
     for ev in event.get():
         if ev.type == QUIT:
             gameRunnning = False
+        if ev.type == KEYDOWN:
+            if ev.key == K_SPACE:
+                bullets.append(Bullet(plr.rect.centerx-2, plr.rect.centery-2, plr.direction))
     plr.update(delta) # update
     window.fill([255,255,255])
-    draw.rect(window, (255,0,0), plr) # draw player
+    draw.rect(window, (255,0,0), plr.rect) # draw player
+    for i in bullets:
+        i.update(delta)
+        draw.rect(window, (0,0,0), i.rect)
     display.update()
