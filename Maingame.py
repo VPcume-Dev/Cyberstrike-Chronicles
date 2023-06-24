ef29ed66ac7b55afb5a27250464c3da9be26688a
VPcume-Dev <vinhphucflash@gmail.com>
2023-06-24 15:24:54 +0700

    fixed phu lon problem

--- Maingame.py
+++ Maingame.py
@@ -11,34 +11,51 @@ green = (181, 230, 29)
 
 game_run = True
 
+class Player():
+    def __init__(self):
+        self.movex = 0
+        self.movey = 0
+        self.direction = "right"
+        self.status = "standing"
+        self.frame_count = 0
+        self.x = 100
+        self.y = 100
+
+    plr_frames = [pygame.image.load("pixels_00.png"), pygame.image.load("pixels_01.png"),
+              pygame.image.load("pixels_02.png"), pygame.image.load("pixels_03.png")]
+
+    def update_sprites(self):
+        window.blit(plr_frames[self.frame_count], (self.x, self.y))
+    
+    def update_frame(self):
+        if self.status == "running":
+            self.frame_count += 1
+            if self.frame_count > 3:
+                self.frame_count = 0
+        if self.status == "standing":
+            self.frame_count = 3
+
+    def player_movement(self, delta):
+        self.x += self.movex * delta
+        self.y += self.movey * delta
+
+Mark = Player()
+
 plr_frames = [pygame.image.load("pixels_00.png"), pygame.image.load("pixels_01.png"),
               pygame.image.load("pixels_02.png"), pygame.image.load("pixels_03.png")]
-delta = pygame.time.Clock().tick(30) / 1000
 
-
-def player_settings():
-    Mark = Player()
-    Mark.movex = 0
-    Mark.movey = 0
-
+def player_settings(delta):
     key = pygame.key.get_pressed()
 
-    if key[K_UP]:
-        Mark.movey = -500
-        Mark.status = "running"
-    if key[K_DOWN]:
-        Mark.movey = 500
-        Mark.status = "running"
-    if key[K_RIGHT]:
-        Mark.movey = 500
-        Mark.status = "running"
-    if key[K_LEFT]:
-        Mark.movey = 500
+    Mark.movex = (key[pygame.K_RIGHT] - key[pygame.K_LEFT]) * 500
+    Mark.movey = (key[pygame.K_DOWN] - key[pygame.K_UP]) * 500
+    
+    if Mark.movex or Mark.movey:
         Mark.status = "running"
 
     Mark.update_frame()
     Mark.update_sprites()
-    Mark.player_movement()
+    Mark.player_movement(delta)
 
 
 def display():
@@ -69,20 +86,21 @@ class Player():
         if self.status == "standing":
             self.frame_count = 3
 
-    def player_movement(self):
+    def player_movement(self, delta):
         self.x += self.movex * delta
         self.y += self.movey * delta
 
 
 while game_run:
+    delta = pygame.time.Clock().tick(30) / 1000
 
     display()
 
-    player_settings()
+    player_settings(delta)
 
     for event in pygame.event.get():
         if event.type == QUIT:
             game_run = False
             pygame.quit()
 
-    pygame.display.flip()
+    pygame.display.update()
